aa66a3f3db113d405bf550ae6c6543390a40f83f
Roman Gorbunov <romangorbunov91@yandex.ru>
2026-02-28 09:13:31 +0700

    Генерация параметров и массивов вынесены в функции.

--- main.py
+++ main.py
@@ -1,84 +1,38 @@
 import numpy as np
-import itertools
 import json
 from pathlib import Path
 
 from utils.settings import var
-from utils.general_functions import transfer_function, complex_to_vect
+from utils.general_functions import transfer_function, generate_masks, generate_freq_zeros_poles
 
 config_dir = Path("./config/")
 
 if __name__ == "__main__":
     
-
     config_path = config_dir / "config.json"
     assert config_path.exists(), f"Config not found: {config_path}"
     with open(config_path, "r") as f:
         configer = json.load(f)
     
-    N = configer["length"]
-    F_MIN_RANGE = configer["fmin"]
-    F_MAX_RANGE = configer["fmax"]
-    N_ZERO_POLE_MAX = configer["Nzp_max"]
-    N_LEFT_POLE_MAX = configer["Nlp_max"]
-    N_RIGHT_POLE_MAX = configer["Nrp_max"]
-    N_LEFT_ZERO_MAX = configer["Nlz_max"]
-    N_RIGHT_ZERO_MAX = configer["Nrz_max"]
+    dataset_dir = Path('./dataset/')
+    output_data_dir = dataset_dir / configer["type"]
+    output_data_dir.mkdir(parents=True, exist_ok=True)
     
+    masks = generate_masks(masks={}, configer=configer)
+
+    # save masks as json.
+    with open(dataset_dir / (configer["type"] + '_masks.json'), "w") as f:
+        json.dump(masks, f, indent=4)
     
-    param_ranges = [
-        range(N_ZERO_POLE_MAX+1),
-        range(N_LEFT_POLE_MAX+1),
-        range(N_RIGHT_POLE_MAX+1),
-        range(N_LEFT_ZERO_MAX+1),
-        range(N_RIGHT_ZERO_MAX+1),
-        range(int(configer["size"]))
-    ]
-    masks = {}
-
-    for nzp, nlp, nrp, nlz, nrz, n in itertools.product(*param_ranges):
-        
-        key = f"{nzp}zp{nlp}lp{nrp}rp{nlz}lz{nrz}rz_{n:03}"
-        
-        masks[key] = {
-            "zero_poles": int(nzp),
-            
-            "left_poles": np.random.choice(
-                N, size=nlp, replace=False),
-
-            "right_poles": np.random.choice(
-                N, size=nrp, replace=False),
-            
-            "left_zeros": np.random.choice(
-                N, size=nlz, replace=False),
-
-            "right_zeros": np.random.choice(
-                N, size=nrz, replace=False)
-        }
-        
-        freq_lim = np.random.uniform(
-            low=[F_MIN_RANGE[0], F_MAX_RANGE[0]],
-            high=[F_MIN_RANGE[-1], F_MAX_RANGE[-1]]
-        )
-        
-        freq = np.linspace(freq_lim[0], freq_lim[-1], N)
-        delta_f = freq[1] - freq[0]
-        
-        poles = []
-        poles.extend(  freq_lim[0] + masks[key]["left_poles"] * delta_f)
-        poles.extend(-(freq_lim[0] + masks[key]["right_poles"] * delta_f))
-        
-        zeros = []
-        zeros.extend(  freq_lim[0] + masks[key]["left_zeros"] * delta_f)
-        zeros.extend(-(freq_lim[0] + masks[key]["right_zeros"] * delta_f))
+    for key, mask in masks.items():
+        freq, zeros, poles = generate_freq_zeros_poles(mask, configer)
         
         gain_complex = transfer_function(
             freq=freq,
-            zero_poles=masks[key]["zero_poles"],
+            zero_poles=mask["zero_poles"],
             poles=poles,
             zeros=zeros)
         
-        out = []
-        out.extend(freq)
-        out.extend(complex_to_vect(gain_complex))
-    print(out[1])
+        data = np.column_stack((freq, np.real(gain_complex), np.imag(gain_complex)))
+
+        np.savetxt(output_data_dir / f"{key}.csv", data, delimiter=',', header='Frequency (Hz), Gain (Real), Gain (Imag')
--- utils/general_functions.py
+++ utils/general_functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import itertools
 from typing import List
 
 def complex_to_mag_db_ph_deg(
@@ -47,12 +48,6 @@ def complex_to_mag_db_ph_deg(
 
     return [mag_db, ph_deg]
 
-def complex_to_vect(
-    gain_complex: List[float]
-) -> List[float]:
-    
-    return np.array(np.real(gain_complex)), np.array(np.imag(gain_complex))
-
 def transfer_function(
   freq: List[float],
   zero_poles: int,
@@ -69,3 +64,68 @@ def transfer_function(
         gain_complex *= 1.0 + 1j*freq/zero
     
     return gain_complex
+
+
+def generate_masks(masks, configer):
+    
+    N = configer["length"]
+    N_ZERO_POLE_MAX = configer["Nzp_max"]
+    N_LEFT_POLE_MAX = configer["Nlp_max"]
+    N_RIGHT_POLE_MAX = configer["Nrp_max"]
+    N_LEFT_ZERO_MAX = configer["Nlz_max"]
+    N_RIGHT_ZERO_MAX = configer["Nrz_max"]
+    
+    param_ranges = [
+        range(N_ZERO_POLE_MAX+1),
+        range(N_LEFT_POLE_MAX+1),
+        range(N_RIGHT_POLE_MAX+1),
+        range(N_LEFT_ZERO_MAX+1),
+        range(N_RIGHT_ZERO_MAX+1),
+        range(int(configer["size"]))
+    ]
+
+    for nzp, nlp, nrp, nlz, nrz, n in itertools.product(*param_ranges):
+        
+        key = f"{nzp}zp{nlp}lp{nrp}rp{nlz}lz{nrz}rz_{n:03}"
+        
+        masks[key] = {
+            "zero_poles": int(nzp),
+            
+            "left_poles": np.random.choice(
+                N, size=nlp, replace=False).tolist(),
+
+            "right_poles": np.random.choice(
+                N, size=nrp, replace=False).tolist(),
+            
+            "left_zeros": np.random.choice(
+                N, size=nlz, replace=False).tolist(),
+
+            "right_zeros": np.random.choice(
+                N, size=nrz, replace=False).tolist()
+        }
+    return masks
+
+
+def generate_freq_zeros_poles(mask, configer):
+
+    N = configer["length"]
+    F_MIN_RANGE = configer["fmin"]
+    F_MAX_RANGE = configer["fmax"]
+    
+    freq_lim = np.random.uniform(
+        low=[F_MIN_RANGE[0], F_MAX_RANGE[0]],
+        high=[F_MIN_RANGE[-1], F_MAX_RANGE[-1]]
+    )
+    
+    freq = np.linspace(freq_lim[0], freq_lim[-1], N)
+    delta_f = freq[1] - freq[0]
+    
+    poles = []
+    poles.extend(  freq_lim[0] + np.array(mask["left_poles"]) * delta_f)
+    poles.extend(-(freq_lim[0] + np.array(mask["right_poles"]) * delta_f))
+    
+    zeros = []
+    zeros.extend(  freq_lim[0] + np.array(mask["left_zeros"]) * delta_f)
+    zeros.extend(-(freq_lim[0] + np.array(mask["right_zeros"]) * delta_f))
+        
+    return freq, zeros, poles
