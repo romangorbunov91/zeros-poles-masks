fc1ad233a4016ba317a28d4445ebf9e894af6fce
rgorbunov <rgorbunov@aedon.ru>
2026-04-15 11:55:39 +0700

    Update.

--- utils/general_functions.py
+++ utils/general_functions.py
@@ -67,7 +67,6 @@ def generate_masks(masks: Dict[str, Any], configer: Dict[str, Any]) -> Dict[str,
     clearance_width = configer["clearance_width"]
     rng = np.random.default_rng(configer["seed"])
     
-    
     for nzp, nlp, nrp, nlz, nrz, n in itertools.product(*param_ranges):
         key = f"{nzp}zp{nlp}lp{nrp}rp{nlz}lz{nrz}rz_{n:03}"
         
@@ -97,8 +96,7 @@ def generate_masks(masks: Dict[str, Any], configer: Dict[str, Any]) -> Dict[str,
                 for idx in range(len(right_objects_borders)//2):
                     right_objects.append(int(rng.integers(right_objects_borders[2*idx], right_objects_borders[2*idx+1])))
             rng.shuffle(right_objects)
-            
-            
+ 
             masks[key] = {
                 "zero_poles": int(nzp),
                 
@@ -112,6 +110,7 @@ def generate_masks(masks: Dict[str, Any], configer: Dict[str, Any]) -> Dict[str,
             }
         
         else:
+            # Overlap allowed.
             masks[key] = {
                 "zero_poles": int(nzp),
                 
